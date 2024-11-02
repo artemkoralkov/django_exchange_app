da886f2897ed2999d9cc034ac1fbab44507037d2
Artem Koralkov <artemkoralkov@gmail.com>
2024-11-02 18:15:02 +0300

    add admin panel options

--- exchange/exchange_app/admin.py
+++ exchange/exchange_app/admin.py
@@ -1,3 +1,21 @@
+# exchange/admin.py
 from django.contrib import admin
+from .models import ExchangeRate, ExchangeTransaction
 
-# Register your models here.
+
+class ExchangeRateAdmin(admin.ModelAdmin):
+    list_display = ('currency_from', 'currency_to', 'rate', 'date')  # Поля, отображаемые в списке
+    search_fields = ('currency_from', 'currency_to')  # Поля для поиска
+    list_filter = ('date',)
+
+
+class ExchangeTransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'operator', 'currency_from', 'currency_to', 'amount', 'exchanged_amount', 'date')  # Поля, отображаемые в списке
+    search_fields = ('operator', 'date', 'currency_from', 'currency_to')  # Поля для поиска
+    list_filter = ('date',)
+
+
+# Регистрация модели
+admin.site.register(ExchangeRate, ExchangeRateAdmin)
+admin.site.register(ExchangeTransaction, ExchangeTransactionAdmin)
