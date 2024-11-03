afbdf99f1a353b7712e80aa3e8f3a671baef5863
Artem Koralkov <artemkoralkov@gmail.com>
2024-11-03 09:34:39 +0300

    fix build.sh

--- exchange/build.sh
+++ exchange/build.sh
@@ -3,11 +3,13 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
+pip install -r ../requirements.txt
+cd ./exchange
 # Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py makemigrations
 python manage.py migrate
+
+python manage.py createsuperuser --noinput
