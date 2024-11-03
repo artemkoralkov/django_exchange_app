11e076ea93876fc14d14efe5749d8a8462ba48cc
Artem Koralkov <artemkoralkov@gmail.com>
2024-11-03 15:25:34 +0300

    moved the secret key to the environment variable

--- exchange/exchange/settings.py
+++ exchange/exchange/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a%pl_f--fn^!qn*q85j((ghkse7*jh_45wc_n9i7w7mcu3g2!e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG'))
