bdbd07cbeba837d9e7aac17ba01a4635497d9321
Artem Koralkov <artemkoralkov@gmail.com>
2024-11-03 11:36:06 +0300

    collect statics in one folder

--- exchange/build.sh
+++ exchange/build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r ../requirements.txt
 #cd ./exchange
 # Convert static asset files
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py makemigrations
